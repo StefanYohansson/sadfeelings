28506cc4f2cd5c3b68d0e9a79f1ebcc434acfd59
StefanYohansson <sy.fen0@gmail.com>
2017-08-25 16:58:10 -0300

    lol stas

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,34 @@
 
 extern crate rocket_contrib;
 extern crate rocket;
+extern crate github_rs;
+
+use github_rs::client::Github;
 
 use std::collections::HashMap;
 use rocket_contrib::Template;
 use rocket::Request;
 
+use std::path::{Path, PathBuf};
+
+use rocket::response::NamedFile;
+
 #[get("/")]
 fn index() -> Template {
-    let mut context: HashMap<&str, &str> = HashMap::new();
+    let context: HashMap<&str, &str> = HashMap::new();
+/*
+    let client = Github::new("API TOKEN");
+    let me = client.get()
+                   .user()
+                   .execute();
+                   */
     Template::render("index", &context)
+}
+
+// @TODO(snotr): receive as many folders and join by /
+#[get("/assets/<folder>/<file..>")]
+fn assets(folder: String, file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new(&format!("assets/{}", folder)).join(file)).ok()
 }
 
 #[error(404)]
@@ -23,7 +42,7 @@ fn not_found(req: &Request) -> Template {
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index, assets])
         .attach(Template::fairing())
         .catch(errors![not_found])
         .launch();
