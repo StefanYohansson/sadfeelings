93f215099f0e6c2da5de78577a7fb19f09ef4946
StefanYohansson <sy.fen0@gmail.com>
2017-09-23 16:51:09 -0300

    Finish first version.
    - add rss
    - finish github
    - add footer

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@
 extern crate rocket_contrib;
 extern crate rocket;
 extern crate github_rs;
+extern crate rss;
 
 use github_rs::client::Github;
+use github_rs::Json;
 
 use std::collections::HashMap;
 use rocket_contrib::Template;
@@ -15,15 +17,54 @@ use std::path::{Path, PathBuf};
 
 use rocket::response::NamedFile;
 
+use rss::{Channel, Item};
+
+fn mock_context<'a>() -> HashMap<&'a str, String> {
+    let mut context: HashMap<&str, String> = HashMap::new();
+    context.insert("email", "snotr@sadfeelings.me".to_string());
+    context.insert("url", "https://github.com/StefanYohansson".to_string());
+    context.insert("avatar_url", "".to_string());
+    context.insert("followers", "41".to_string());
+    context.insert("followers_url", "https://github.com/StefanYohansson/followers".to_string());
+    context
+}
+
+fn extract_json(json: &Json, key: &str) -> String {
+    json.get(key).unwrap().to_string().trim_matches('"').to_string()
+}
+
 #[get("/")]
 fn index() -> Template {
-    let context: HashMap<&str, &str> = HashMap::new();
-/*
-    let client = Github::new("API TOKEN");
-    let me = client.get()
+    let mut context: HashMap<&str, String> = HashMap::new();
+    // @TODO: move token to config
+    // it's a read only token, so it won't be a problem
+    let client = Github::new("2538a3824751f390882c5c1edd06fa7f30953c9f");
+    let me = client.unwrap().get()
                    .user()
                    .execute();
-                   */
+
+    match me {
+        Ok((headers, status, json)) => {
+            if let Some(json) = json{
+                context.insert("email", extract_json(&json, "email"));
+                context.insert("url", extract_json(&json, "html_url"));
+                context.insert("avatar_url", extract_json(&json, "avatar_url"));
+                context.insert("followers", extract_json(&json, "followers"));
+                context.insert("followers_url", extract_json(&json, "followers_url"));
+            }
+        },
+        Err(e) => println!("{}", e)
+    }
+
+    let channel = Channel::from_url("http://snotr.sadfeelings.me/feed.xml").unwrap();
+    let item = channel.items().first();
+    match item {
+        Some(i) => {
+           context.insert("latest_title", i.title().unwrap().to_string()); 
+           context.insert("latest_link", i.link().unwrap().to_string()); 
+        },
+        None => println!("no feed available")
+    }
     Template::render("index", &context)
 }
 
